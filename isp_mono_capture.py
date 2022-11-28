949f05a0e6d845aca02c27f6adcea2053f886a04
Priya Jakhar <80418210+priyajakhar@users.noreply.github.com>
2022-11-28 13:02:39 -0500

    Update isp_mono_capture.py
    # added option of setting manual focus for color cameras
    # default value is -1 which makes sure that if the argument is not passed, the focus setting would be on AUTO for the camera

--- isp_mono_capture.py
+++ isp_mono_capture.py
@@ -19,6 +19,7 @@ argn.add_argument('-iso', type=int, default=400)   # iso of color camera
 argn.add_argument('-fps', type=float, default=1)    # fps of all cameras
 argn.add_argument('-ss', type=float, default=1)   # shutter speed of color camera in milliseconds
 argn.add_argument('-miso', type=int, default=150)   # iso of mono camera
+argn.add_argument('-focus', type=int, default=-1)   # focus of color camera (0 for far to 255 for near)
 args = argn.parse_args()
 mono_ss = 1000
 
@@ -29,18 +30,23 @@ pipeline = dai.Pipeline()
 camRgb = pipeline.create(dai.node.ColorCamera)
 camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
 camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_12_MP)
-camRgb.setInterleaved(False)
 camRgb.initialControl.setSharpness(0)     # range: 0..4, default: 1		
 camRgb.initialControl.setLumaDenoise(0)   # range: 0..4, default: 1		
 camRgb.initialControl.setChromaDenoise(4) # range: 0..4, default: 1
+# expTimeUs = int(round(args.ss * 1000))
+# camRgb.initialControl.setManualExposure(expTimeUs, args.iso)
+camRgb.setFps(args.fps)
+# camRgb.initialControl.setManualFocus(100)
 
 # Mono cameras
 monoRight = pipeline.create(dai.node.MonoCamera)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoRight.setFps(args.fps)
 monoLeft = pipeline.create(dai.node.MonoCamera)
 monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoLeft.setFps(args.fps)
 
 # depth
 Depth = pipeline.create(dai.node.StereoDepth)
@@ -49,6 +55,7 @@ Depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
 Depth.setLeftRightCheck(True)
 Depth.setExtendedDisparity(False)
 Depth.setSubpixel(False)
+Depth.setNumFramesPool(1)
 
 ## Linking
 xoutRgb = pipeline.create(dai.node.XLinkOut)
@@ -70,31 +77,35 @@ monoLeft.out.link(Depth.left)
 Depth.disparity.link(xoutDepth.input)
 
 xin1 = pipeline.create(dai.node.XLinkIn)
-xin1.setMaxDataSize(1)
-xin1.setStreamName("controlr")
-xin1.out.link(camRgb.inputControl)
+xin1.setNumFrames(1)	
+xin1.setMaxDataSize(1)	
+xin1.setStreamName("controlr")	
+xin1.out.link(camRgb.inputControl)	
 
-xin2 = pipeline.create(dai.node.XLinkIn)
-xin2.setMaxDataSize(1)
-xin2.setStreamName("controlm")
-xin2.out.link(monoLeft.inputControl)
+xin2 = pipeline.create(dai.node.XLinkIn)	
+xin2.setNumFrames(1)
+xin2.setMaxDataSize(1)	
+xin2.setStreamName("controlm")	
+xin2.out.link(monoLeft.inputControl)	
 xin2.out.link(monoRight.inputControl)
 
 
-def set_ss_iso(expTimeMs, sensIso):
-    expTimeUs = int(round(expTimeMs * 1000))
-    ctr1 = dai.CameraControl()
-    ctr2 = dai.CameraControl()
-    ctr1.setManualExposure(expTimeUs, sensIso)
-    qControl1.send(ctr1)
-    ctr2.setManualExposure(mono_ss, args.miso)
+def set_ss_iso(expTimeMs, sensIso):	
+    expTimeUs = int(round(expTimeMs * 1000))	
+    ctr1 = dai.CameraControl()	
+    ctr2 = dai.CameraControl()	
+    ctr1.setManualExposure(expTimeUs, sensIso)	
+    qControl1.send(ctr1)	
+    ctr2.setManualExposure(mono_ss, args.miso)	
     qControl2.send(ctr2)
 
 
-def set_fps(fps):
-    camRgb.setFps(fps)
-    monoRight.setFps(fps)
-    monoLeft.setFps(fps)
+def manualFocus(focus):
+    if focus < 0: return
+    assert(focus >= 0 and focus <= 255)
+    ctrl = dai.CameraControl()
+    ctrl.setManualFocus(focus)
+    qControl1.send(ctrl)
 
 
 # Connect to device and start pipeline
@@ -107,13 +118,13 @@ with dai.Device(pipeline) as device:
     qLeft = device.getOutputQueue(name="left", maxSize=5, blocking=False)
     qRGB = device.getOutputQueue(name="rgb", maxSize=5, blocking=False)
     qDepth = device.getOutputQueue(name="disparity", maxSize=5, blocking=False)
-    qControl1 = device.getInputQueue(name="controlr")
+    qControl1 = device.getInputQueue(name="controlr")	
     qControl2 = device.getInputQueue(name="controlm")
-
-    # iso and shutter speed
-    set_ss_iso(args.ss, args.iso)
-    # fps
-    set_fps(args.fps)
+    	    	
+    # iso and shutter speed	
+    set_ss_iso(args.ss, args.iso)	
+    # focus	
+    manualFocus(args.focus)
 
     for r in range(30):
         c, r, l, d = qRGB.get(), qRight.get(), qLeft.get(), qDepth.get()
@@ -121,8 +132,7 @@ with dai.Device(pipeline) as device:
     print("Started")
     start = time.time()
 
-    i = 0
-    while(i<n):
+    for i in range(n):
         t = str(time.time())
 
         inRgb = qRGB.tryGet()
@@ -140,6 +150,4 @@ with dai.Device(pipeline) as device:
             dframe = (dframe * init_dsp).astype(np.uint8)
             cv2.imwrite(f"{dirName}/{t}_Depth.png", dframe)
         
-        i += 1
-
 print("Finished in", round(time.time()-start, 2))
