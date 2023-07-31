24114a1ef3b18f932c900ae79754e6b71a83faca
Priya Jakhar <priyaftmage@gmail.com>
2023-07-31 11:10:29 -0400

    remove older files
    remove and update files

--- socket/client.py
+++ socket/client.py
@@ -1,29 +1,104 @@
 import socket, cv2, pickle, struct
+import json
+import sys
+import numpy as np
+import json_numpy
+import time
+import select
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_ip = '192.168.181.1'
 port = 6666
-client_socket.connect((host_ip, port))
 
-data = b""
-payload_size = struct.calcsize("Q")
+# for starting up the camera on server side
+str_cmd = { "key":"Start" }
+str_msg = json.dumps(str_cmd)
+# for getting a preview frame from camera
+img_cmd = { "key":"Preview" }
+img_msg = json.dumps(img_cmd)
+# for getting a set of images (1 RGB + 2 Mono + 1 Depth)
+snap_cmd = { "key":"TakeSnapshot" }
+snap_msg = json.dumps(snap_cmd)
+# for stopping the camera on server side
+img_scmd = { "key":"StopPreview" }
+img_smsg = json.dumps(img_scmd)
+# for closing the client connection to the server
+stp_cmd = { "key":"Close" }
+stp_msg = json.dumps(stp_cmd)
 
-while True:
-    while len(data) < payload_size:
-        packet = client_socket.recv(4*1024)
-        if not packet:	break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-    while len(data) < msg_size:
-        data += client_socket.recv(4*1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-	
-    cv2.imshow('Received', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-client_socket.close()
+for i in range(1):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host_ip, port))
+
+    # start preview
+    print("\nStart")
+    message = str_msg.encode()
+    client_socket.sendall(message)
+    time.sleep(5)
+
+    # preview messages
+    message = img_msg.encode()
+    j = time.time()
+    
+    while(time.time()-j < 5):
+        client_socket.sendall(message)
+        t = time.time()
+        # get length of message
+        data = b""
+        data = client_socket.recv(7)
+        l = int( data.decode() )
+        # get actual frame
+        data = b""
+        while len(data) < l:
+            packet = client_socket.recv(1024)
+            data += packet
+
+        arr = data.decode()
+        decoded_arr = json.loads(arr)
+        dd = decoded_arr["data"]["pixels"]
+        cv2.imwrite(f"preview/prv_{t}.jpg", np.asarray(dd))
+
+
+    # ask for a snapshot
+    message = snap_msg.encode()
+    client_socket.sendall(message)
+    t = time.time()
+
+    data = b""
+    for i in range(3):
+
+        while len(data) < 7:
+            packet = client_socket.recv(1024)
+            if not packet:	break
+            data += packet
+        msg_size = json.loads(data[:7].decode())
+        l = int(msg_size)
+        print(l)
+        data = data[7:]
+        while len(data) < l:
+            data += client_socket.recv(1024)
+        decoded_arr = json.loads(data[:l].decode())
+        data = data[l:]
+    
+        dd = decoded_arr["data"]["pixels"]
+    
+        if (decoded_arr["type"]=="RGB"):
+            cv2.imwrite(f"preview/rgb_{t}.jpg", np.asarray(dd))
+        if (decoded_arr["type"]=="MonoLeft"):
+            cv2.imwrite(f"preview/left_{t}.jpg", np.asarray(dd))
+        if (decoded_arr["type"]=="MonoRight"):
+            cv2.imwrite(f"preview/right_{t}.jpg", np.asarray(dd))
+
+
+    # stop preview
+    print("\nStop Camera")
+    message = img_smsg.encode()
+    client_socket.sendall(message)
+
+
+    # closing connection
+    print("Close")
+    message = stp_msg.encode()
+    client_socket.sendall(message)
+    client_socket.close()
+    print("Client Closed")
--- socket/server.py
+++ socket/server.py
@@ -1,49 +1,243 @@
-import cv2
 import depthai as dai
-import sys
+import numpy as np
+import json
+import socket
+import time
 
-import socket, pickle, struct
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
-print('HOST IP:', host_ip)
 port = 6666
 socket_address = (host_ip, port)
 server_socket.bind(socket_address)
 server_socket.listen(5)
 print("Listening at:", socket_address)
 
+
+err_dict = {
+	"key":"Error",
+	"data":None,
+	"error": {
+		"message": "",
+		"cause": ""
+	}
+}
+
+prv_dict = {
+	"key":"Image",
+	"type": "Preview",
+	"data":{
+		"length":270000,
+		"width":300,
+		"height":300,
+		"pixels":[]
+	},
+	"error": None
+}
+
+img_dict_c = {
+	"key":"Image",
+	"type": "RGB",
+	"data":{
+		"length":12257280,
+		"width":4032,
+		"height":3040,
+		"pixels":[]
+	},
+	"error": None
+}
+
+img_dict_l = {
+	"key":"Image",
+	"type": "MonoLeft",
+	"data":{
+		"length":921600,
+		"width":1280,
+		"height":720,
+		"pixels":[]
+	},
+	"error": None
+}
+
+img_dict_r = {
+	"key":"Image",
+	"type": "MonoRight",
+	"data":{
+		"length":921600,
+		"width":1280,
+		"height":720,
+		"pixels":[]
+	},
+	"error": None
+}
+
+img_dict_d = {
+	"key":"Image",
+	"type": "Depth",
+	"state": "Progress",
+	"data":{
+		"length":921600,
+		"width":1280,
+		"height":720,
+		"pixels":[]
+	},
+	"error": None
+}
+
+
+# OAK-D pipeline
 pipeline = dai.Pipeline()
+
+# RGB
 camRgb = pipeline.create(dai.node.ColorCamera)
-camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-camRgb.setPreviewSize(640, 480)
-xoutRgb = pipeline.create(dai.node.XLinkOut)
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_12_MP)
+
+manip = pipeline.create(dai.node.ImageManip)
+manip.initialConfig.setCropRect(0.006, 0, 1, 1)
+manip.setNumFramesPool(2)
+manip.setMaxOutputFrameSize(18385920)
+manip.initialConfig.setFrameType(dai.ImgFrame.Type.NV12)
+camRgb.isp.link(manip.inputImage)
+
+videoEnc = pipeline.create(dai.node.VideoEncoder)
+videoEnc.setDefaultProfilePreset(1, dai.VideoEncoderProperties.Profile.MJPEG)
+manip.out.link(videoEnc.input)
+
+xoutRgb = pipeline.createXLinkOut()
 xoutRgb.setStreamName("rgb")
-camRgb.preview.link(xoutRgb.input)
+videoEnc.bitstream.link(xoutRgb.input)
 
-with dai.Device(pipeline) as device:
-    print('Connected')
-    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    qt = False
-    while True:
-        client_socket, addr = server_socket.accept()
-        print('Got Connection from:', addr)
+xoutRgbp = pipeline.createXLinkOut()
+xoutRgbp.setStreamName("preview")
+camRgb.preview.link(xoutRgbp.input)
 
-        if client_socket:
-            while True:
-                inRgb = qRgb.get()
-                frame = inRgb.getCvFrame()
-                a = pickle.dumps(frame)
-                message = struct.pack("Q", len(a))+a
-                client_socket.sendall(message)
-                cv2.imshow('Tranmitting video', frame)
-                if cv2.waitKey(1) == ord('q'):
-                    client_socket.close()
-                    qt = True
+# monoRight
+monoRight = pipeline.create(dai.node.MonoCamera)
+xoutRight = pipeline.create(dai.node.XLinkOut)
+xoutRight.setStreamName("right")
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoRight.out.link(xoutRight.input)
+
+# monoLeft
+monoLeft = pipeline.create(dai.node.MonoCamera)
+xoutLeft = pipeline.create(dai.node.XLinkOut)
+xoutLeft.setStreamName("left")
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoLeft.out.link(xoutLeft.input)
+
+# depth
+Depth = pipeline.create(dai.node.StereoDepth)
+Depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+Depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+Depth.setLeftRightCheck(True)
+Depth.setExtendedDisparity(False)
+Depth.setSubpixel(False)
+
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+xoutDepth.setStreamName("disparity")
+monoRight.out.link(Depth.right)
+monoLeft.out.link(Depth.left)
+Depth.disparity.link(xoutDepth.input)
+
+
+
+for p in range(1):
+    client_socket, addr = server_socket.accept()
+    print('Got Connection from:', addr)
+
+    if client_socket:
+        while True:
+            try:
+                cmd = client_socket.recv(1024)
+                if not cmd:
                     break
-        if qt:
-            break
+            except:
+                print("Connection dropped by client")
+                break
+            
+            try:
+                d_rcv = json.loads(cmd)
+            except Exception:
+                print("Invalid message")
+                break
 
-print("Program Ended")
+            c_key = d_rcv.get("key")
 
+            if(c_key == "Start"):
+                print("\nStarting camera")
+            
+                with dai.Device(pipeline) as device:
+                    queue = device.getOutputQueue(name="preview", maxSize=2, blocking=False)
+                    qRGB = device.getOutputQueue(name="rgb", maxSize=2, blocking=False)
+                    qRight = device.getOutputQueue(name="right", maxSize=2, blocking=False)
+                    qLeft = device.getOutputQueue(name="left", maxSize=2, blocking=False)
+                    qDepth = device.getOutputQueue(name="disparity", maxSize=2, blocking=False)
+                    init_dsp = (255 / Depth.initialConfig.getMaxDisparity())
+            
+                    while True:
+                        try:
+                            cmd = client_socket.recv(1024)
+                            d_rcv = json.loads(cmd)
+                        except:
+                            err_dict["error"]["message"] = "Error with message received"
+                            j_e = json.dumps(err_dict).encode()
+                            client_socket.sendall(j_e)
+                            break
+                        c_key = d_rcv.get("key")
+
+                        if(c_key != "StopPreview"):
+                            if(c_key == "Preview"):
+                                imOut = queue.get().getCvFrame()
+                                prv_dict["data"]["pixels"] = imOut.tolist()
+                                j_p = json.dumps(prv_dict)
+
+                            if(c_key == "TakeSnapshot"):
+                                inRgb = qRGB.get().getData()
+                                inLeft = qLeft.get().getFrame()
+                                inRight = qRight.get().getFrame()
+                                inDepth = qDepth.get().getFrame()
+                                img_dict_c["data"]["pixels"] = inRgb.tolist()
+                                img_dict_l["data"]["pixels"] = inLeft.tolist()
+                                img_dict_r["data"]["pixels"] = inRight.tolist()
+                                dframe = (inDepth * init_dsp).astype(np.uint8)
+                                img_dict_d["data"]["pixels"] = dframe.tolist()
+                                continue
+
+                            if(c_key == "RGB"):
+                                j_p = json.dumps(img_dict_c)
+                            if(c_key == "MonoLeft"):
+                                j_p = json.dumps(img_dict_l)
+                            if(c_key == "MonoRight"):
+                                j_p = json.dumps(img_dict_r)
+                            if(c_key == "Depth"):
+                                j_p = json.dumps(img_dict_d)
+
+                            try:
+                                message = j_p.encode()
+                                ln = (f'{len(message):<10}').encode()
+                                client_socket.sendall(ln)
+                                client_socket.sendall(message)
+                            except:
+                                err_dict["error"]["message"] = "Issue with fetching image from camera"
+                                j_e = json.dumps(err_dict).encode()
+                                try:
+                                    client_socket.sendall(j_e)
+                                except:
+                                    print("Client connection dropped")
+                                    break
+                                continue
+
+                        else:
+                            print("Stoping Camera")
+                            break
+
+            if(c_key == "Close"):
+                print("Closing Connection")
+                client_socket.close()
+                time.sleep(1)
+                print("Connection closed\n")
+                break
